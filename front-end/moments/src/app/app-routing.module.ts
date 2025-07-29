bca9b8d59c8dbcc338d249916947e480b1a450f2
Lucas williams <lucaswilliamscesar@gmail.com>
2025-07-29 06:35:50 -0300

    created estruct of project

--- front-end/moments/src/app/app-routing.module.ts
+++ front-end/moments/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/pages/home/home.component';
 import { AboutComponent } from './Components/pages/about/about.component';
-
+import { NewMomentComponent } from './Components/pages/new-moment/new-moment.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent },
+  { path: 'moments/new', component: NewMomentComponent}
 ];
 
 @NgModule({
--- front-end/moments/src/app/app.module.ts
+++ front-end/moments/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { AboutComponent } from './Components/pages/about/about.component';
 import { HomeComponent } from './Components/pages/home/home.component';
+import { MomentFormComponent } from './Components/moment-form/moment-form.component';
+import { NewMomentComponent } from './Components/pages/new-moment/new-moment.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,10 @@ import { HomeComponent } from './Components/pages/home/home.component';
     HeaderComponent,
     FooterComponent,
     AboutComponent,
-    HomeComponent
+    HomeComponent,
+    MomentFormComponent,
+    NewMomentComponent
+    
   ],
   imports: [
     BrowserModule,
